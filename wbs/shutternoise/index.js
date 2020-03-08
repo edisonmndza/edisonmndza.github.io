a43b0c757f4fe20e0b1433acec9c16a3bf8d5254
Edison Mendoza <38051583+edisonmndza@users.noreply.github.com>
2020-03-08 13:14:45 -0600

    80% Done for a3
    TODO:
    Chat log history: / 10 points
    	•	The server should remember at least the last 200 messages. It is ok to store this history in memory rather than persisting it on disk.
    	•	When a user connects to the system, the entire chat log should be displayed to the user.
    
    Cookies: / 10 points
    	•	If the user gets disconnected and then the user reconnects again, the user should be assigned the same nickname. You can use browser cookies for this purpose.
    	•	You may find it easier to work with cookies using client-side JavaScript. Some information regarding this is available here: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
    	•	If the nickname was taken in the meantime, the user should get a random nick.

--- wbs/shutternoise/index.js
+++ wbs/shutternoise/index.js
@@ -3,21 +3,105 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let userList = [];
+let chatLog = [[]];
+let userCount = 0;
+
 app.use(express.static('public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-      });
+    //when user connects
+    socket.emit ('user-connect')
+
+    socket.on('new-user', function(){
+      let userDefault = 'user' + userCount
+      userCount++;
+      //add new user to userList
+      userList.push(userDefault);
+
+      //add an identifier for this socket for later use
+      socket.name = userDefault;
+      socket.color = '000000';
+
+      //let user now who he is
+      socket.emit('assigned-name', userDefault)
+
+      //notify everyone about new user
+      var res = '<span style="color:#' + socket.color + '"> ' + socket.name + '</span> has entered the room.'
+      io.emit('add-message', '', '', getTime(), res, 0)
+      
+      //update everyone's user list
+      io.emit('update-user-list', userList)
+    })
+
+    socket.on('add-message', function(msg){
+      // check for nickname change
+      if(msg.startsWith("/nick ")){
+        if (userList.indexOf(msg.substring(6)) == -1){
+          //Notify everyone
+          var res = 'Server: <span style="color:#' + socket.color + '"> ' + socket.name + '</span>' + 
+                    ' renamed to' + 
+                    '<span style="color:#' + socket.color + '"> ' + msg.substring(6) + '</span>';
+          io.emit('add-message', socket.color, socket.name, getTime(), res, 3);
+
+          //Remove and replace from list & socket name
+          userList.splice(userList.indexOf(socket.name), 1);
+          socket.name = msg.substring(6)
+          userList.push(socket.name);
+
+          //Change saved name on clientside
+          socket.emit('assigned-name', socket.name)
+
+          //Update all client list
+          io.emit('update-user-list', userList)
+
+        } else {
+          var res = 'Server: Sorry, ' + msg.substring(6) + ' is already taken. Please try again.';
+          socket.emit('add-message', '', '', getTime(), res, 0);
+        }
+      } // check for color change
+      else if (msg.startsWith("/nickcolor ")){
+        //change color
+        var holder = socket.color
+        socket.color = msg.substring(11);
+        
+        //Notify everyone
+        var res = 'Server: <span style="color:#' + holder + '"> ' + socket.name + '</span>' + 
+                          ' color changed to' + 
+                          '<span style="color:#' + socket.color + '"> ' + socket.name + '</span>';
+        io.emit('add-message', '', '', getTime(), res, 3);
+
+      } else {        
+        //send to everyone else
+        socket.broadcast.emit('add-message', socket.color, socket.name, getTime(), msg, 2);
+        //send to you but bolded
+        socket.emit('add-message', socket.color, socket.name, getTime(), msg, 1)
+      }
+    });
+
     socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
+      // find user in the list and remove from list
+      userList.splice(userList.indexOf(socket.name), 1);
+
+      // update everyone's list
+      io.emit('update-user-list', userList)
+      //notify everyone that user left
+      var res = '<span style="color:#' + socket.color + '"> ' + socket.name + '</span> has left the room.'
+      io.emit('add-message', '', '', getTime(), res, 0)
+    });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function getTime(){
+  var h = new Date().getHours()
+  var m = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes()
+  var currTime = h + ':' + m
+
+  return currTime
+}
--- wbs/shutternoise/public/client.js
+++ wbs/shutternoise/public/client.js
@@ -1,16 +1,61 @@
+let username;
+
 $(function () {
   var socket = io();
+  
   $('form').submit(function(e){
-  e.preventDefault(); // prevents page reloading
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
-  return false;
-  });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
-    scrollToBottom();
+    e.preventDefault(); // prevents page reloading
+    socket.emit('add-message', $('#m').val());
+    $('#m').val('');
+    return false;
   });
 
+  socket.on('user-connect', function(){
+    socket.emit('new-user');
+  })
+
+  socket.on('assigned-name', function(myName){
+    username = myName;
+  })
+
+  socket.on('update-user-list', function(userList){
+    //reset user list
+    $('#user-list').text("");
+
+    //add all users
+    userList.forEach(function(currUser, ind, arr){
+      if (currUser === username) {
+        $('#user-list').append($('<li>').html('<b>'+currUser+' (you)</b>'))
+      } else {
+        $('#user-list').append($('<li>').text(currUser))
+      }
+    })
+  })
+
+  // type 0 = user activity (join/left, name/color change)
+  // type 1 = user's message
+  // type 2 = others' message
+  // type 3 = server's message
+  socket.on('add-message', function(color, sender, timeStamp, msg, type){
+    switch (type){
+      case 0:
+        $('#messages').append($('<li>').html('[' + timeStamp + ']<i> ' + msg + '</i>'));
+        break;
+      case 1:
+        $('#messages').append($('<li>').html('[' + timeStamp + ']<b> ' + 
+        '<span style="color:#' + color + '">' + sender + '</span>: ' + 
+        msg + '</b>'));
+        break;
+      case 2: 
+        $('#messages').append($('<li>').html( '[' + timeStamp + '] ' + 
+        '<span style="color:#' + color + '">' + sender + '</span>: '  + msg));
+        break; 
+        case 3:
+          $('#messages').append($('<li>').html('[' + timeStamp + '] ' + msg));
+          break;
+    }
+    scrollToBottom();
+  });
 });
 
 function scrollToBottom(){
